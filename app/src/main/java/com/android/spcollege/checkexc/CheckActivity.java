c8860fc714b0ea182461418e5e09eb4b0314eac0
Long Huynh <huynhkimlong1993@gmail.com>
2017-09-21 18:07:45 -0400

    Assignment 4

--- app/src/main/java/com/android/spcollege/checkexc/CheckActivity.java
+++ app/src/main/java/com/android/spcollege/checkexc/CheckActivity.java
@@ -1,7 +1,14 @@
 package com.android.spcollege.checkexc;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+
+import static com.android.spcollege.checkexc.R.layout.fragment_session;
 
 public class CheckActivity extends AppCompatActivity {
 
@@ -10,4 +17,20 @@ public class CheckActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_check);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu m) {
+        getMenuInflater().inflate(R.menu.logout,m);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item){
+        int id = item.getItemId();
+        if (id == R.id.action_settings){
+            startActivity(new Intent(CheckActivity.this, LoginActivity.class));
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 }
+
